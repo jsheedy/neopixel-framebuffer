aca3303c97e0770305b0bc6d67697c926472cc7b
Joseph Sheedy <joseph.sheedy@gmail.com>
2017-06-16 14:05:04 -0700

    minimal effort in update

--- fx/yellowblackandredgreenpurple.py
+++ fx/yellowblackandredgreenpurple.py
@@ -1,9 +1,10 @@
+import itertools
 from .fx import Fx
 
 
 class YellowBlackAndRedGreenPurple(Fx):
 
-    colors = (
+    _colors = (
         [255,255,0] * 20,
         [0,0,0] * 20,
         [255,0,0] * 20,
@@ -11,10 +12,15 @@ class YellowBlackAndRedGreenPurple(Fx):
         [102,51,153] * 20
     )
 
-    width = 20
-    # list(zip(range(6), itertools.cycle(range(3))))
+    def __init__(self, *args, width=20, start_indexes=(0, 105, 210, 320),**kwargs):
+        super().__init__(*args, **kwargs)
+        slices = [
+            slice((x + i*width)*3,(x + (i+1)*width)*3)
+            for x in start_indexes
+            for i in range(len(self._colors))
+        ]
+        self._slice_colors = list(zip(slices, itertools.cycle(self._colors)))
 
     def _update(self):
-        for start_index in (0, 105, 210, 320):
-            for i, color in enumerate(self.colors):
-                self.video_buffer.buffer[(start_index + i*self.width)*3:(start_index + (i+1)*self.width)*3] = color
+        for slice, color in self._slice_colors:
+            self.video_buffer.buffer[slice] = color
