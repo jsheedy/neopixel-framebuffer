fixed matrix scaling, rotate * time.deltaTime

fx/matrix.py
@@ -9,13 +9,8 @@ class Matrix(Fx):
         super().__init__(video_buffer, **kwargs)
 
 
-    # def set(self, addr, value, color :str ='r'):
-    #     self.rgb[color] = value
-    #     self.dirty = True
-
-
     def _update(self):
-        theta = self.video_buffer.t / 10
+        theta = self.video_buffer.delta_time
         Rx = np.matrix([
             [1, 0, 0],
             [0, np.cos(theta), -np.sin(theta)],
@@ -31,16 +26,17 @@ class Matrix(Fx):
             [np.sin(theta), np.cos(theta), 0],
             [0, 0, 1],
         ])
+        s = .5 / np.sqrt(2)
         S = np.matrix([
-            [0.5, 0, 0],
-            [0, 0.5, 0],
-            [0, 0, 0.5]
+            [s, 0, 0],
+            [0, s, 0],
+            [0, 0, s]
         ])
         T = np.matrix([
             [1, 0, .5],
             [0, 1, .5],
             [0, 0, .5],
         ])
-        R = Rz  # Ry * Rz * Rx
+        R = Rz * Ry * Rx
         Z = R * T * S * np.matrix(self.video_buffer.buffer).T
         self.video_buffer.buffer = (S.I * T.I * Z).T.A

video_buffer.py
@@ -21,6 +21,7 @@ class VideoBuffer(object):
         self.effects = OrderedDict()
         self.t0 = datetime.now()
         self.t = 0.0
+        self.delta_time = 0.0
         self.enabled = True
         self.device_x = np.linspace(0, 1, N)
         self.buffer_x = np.linspace(0, 1, resolution)
@@ -68,7 +69,9 @@ class VideoBuffer(object):
         # brightness
         self.buffer -= (1.0-self.brightness)
 
-        self.t = (datetime.now() - self.t0).total_seconds()
+        t = (datetime.now() - self.t0).total_seconds()
+        self.delta_time = t - self.t
+        self.t = t
         self.frame += 1
         return self.frame
 