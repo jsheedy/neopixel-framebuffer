a5ccb276fc78df3141c5fb5d6f4a8201067843a4
Joseph Sheedy <joseph.sheedy@gmail.com>
2018-10-13 11:27:28 -0700

    only update background when necessary

--- fx/background.py
+++ fx/background.py
@@ -8,12 +8,15 @@ class BackGround(Fx):
     def __init__(self, video_buffer, color=(1,0,0), **kwargs):
         super().__init__(video_buffer, **kwargs)
         self.rgb = OrderedDict(zip('rgb', color))
-
+        self.dirty = True
 
     def set(self, addr, value, color :str ='r'):
         self.rgb[color] = value
+        self.dirty = True
 
 
     def _update(self):
-        self.x[:] = tuple(self.rgb.values())
+        if self.dirty:
+            self.x[:] = tuple(self.rgb.values())
+            self.dirty = False
         return self.x
